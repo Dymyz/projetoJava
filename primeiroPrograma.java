079ac87dbca83997e7fe139746bbd9ee3b444a35
Dymyz <114318435+Dymyz@users.noreply.github.com>
2023-08-24 12:59:18 -0300

    Add files via upload

---
+++ primeiroPrograma.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class primeiroPrograma {
+    public static void main(String[] args) throws Exception {
+        Date data = new Date();
+        System.out.println("o horario e");
+        System.out.println(data.toString());
+    }
+    
+}
